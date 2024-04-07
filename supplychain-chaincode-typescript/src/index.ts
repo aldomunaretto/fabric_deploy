f293d0feaf1152157d942133f5a1682338dcc382
Aldo Munaretto <aldomunaretto@gmail.com>
2024-04-07 16:05:28 +0000

    Limpiando repositorio

--- supplychain-chaincode-typescript/src/index.ts
+++
@@ -1,9 +0,0 @@
-/*
- * SPDX-License-Identifier: Apache-2.0
- */
-
-import {ProductContract} from './contract';
-
-export {ProductContract} from './contract';
-
-export const contracts: any[] = [ProductContract]
